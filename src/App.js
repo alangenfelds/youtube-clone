6b6a71fafff55143eec9edf072a8641503856e7e
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-01-07 12:09:49 +0200

    added component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
